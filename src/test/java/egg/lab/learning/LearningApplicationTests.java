cf27767c4a3f54fa6a5849ef022268a06cf4ec7a
俩二蛋 <ken442266@gmail.com>
2021-03-16 08:42:43 +0800

    DS: Adding DataSource && Test connection

--- src/test/java/egg/lab/learning/LearningApplicationTests.java
+++ src/test/java/egg/lab/learning/LearningApplicationTests.java
@@ -1,13 +1,19 @@
 package egg.lab.learning;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class LearningApplicationTests {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+        jdbcTemplate.execute("select 1");
     }
 
 }
