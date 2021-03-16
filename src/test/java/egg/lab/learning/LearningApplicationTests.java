184b581a50b6cf554daedd58af3ca898a9e4e549
俩二蛋 <ken442266@gmail.com>
2021-03-16 08:31:02 +0800

    PRJ: Initial a Empty Project

---
+++ src/test/java/egg/lab/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package egg.lab.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
